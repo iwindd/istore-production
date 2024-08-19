51bd0648ec3e651a5e282767fcbe6ee88d72d7e6
iwindd <freewinendlife@gmail.com>
2024-08-19 21:42:18 +0700

    feat: server-action lib

---
+++ src/lib/action.ts
@@ -0,0 +1,25 @@
+import { FieldError } from "react-hook-form";
+import { z, ZodIssue } from "zod";
+
+export interface ActionResponse<T>{
+  success: boolean,
+  data: T,
+  errors?: Record<keyof T, ZodIssue>,
+  message?: string
+}
+
+export const ActionError = (error: any) => {
+  if (error instanceof z.ZodError) {
+    // Handle Zod validation errors
+    return {
+      success: false,
+      errors: error.errors, // Passes validation errors to the client
+    };
+  } else {
+    // Handle other errors (e.g., network, database)
+    return {
+      success: false,
+      message: "An unexpected error occurred.",
+    };
+  }
+};
