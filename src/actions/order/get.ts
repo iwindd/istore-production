68ae52ec425abf28e83e6dcf30ab708295fe0118
iwindd <freewinendlife@gmail.com>
2024-08-20 19:06:31 +0700

    feat<page>: histories

---
+++ src/actions/order/get.ts
@@ -0,0 +1,44 @@
+"use server";
+import { TableFetch } from "@/components/Datatable";
+import { ActionError, ActionResponse } from "@/lib/action";
+import db from "@/lib/db";
+import { order } from "@/lib/formatter";
+import { getServerSession } from "@/lib/session";
+import { Order } from "@prisma/client";
+
+const GetHistories = async (
+  table: TableFetch
+): Promise<ActionResponse<Order[]>> => {
+  try {
+    const session = await getServerSession();
+    const histories = await db.$transaction([
+      db.order.findMany({
+        skip: table.pagination.page * table.pagination.pageSize,
+        take: table.pagination.pageSize,
+        orderBy: order(table.sort),
+        where: {
+          store_id: Number(session?.user.store),
+        },
+      }),
+      db.order.count({
+        where: {
+          store_id: Number(session?.user.store),
+        },
+      }),
+    ]);
+
+    console.log(histories[0]);
+    
+    return {
+      success: true,
+      data: histories[0],
+      total: histories[1],
+    };
+  } catch (error) {
+    console.log(error);
+    
+    return ActionError(error) as ActionResponse<Order[]>;
+  }
+};
+
+export default GetHistories;
---
+++ src/app/histories/components/datatable.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React from "react";
+import * as ff from "@/lib/formatter";
+import { ViewAgendaTwoTone } from "@mui/icons-material";
+import { Category } from "@prisma/client";
+import GridLinkAction from "@/components/GridLinkAction";
+import { GridColDef } from "@mui/x-data-grid";
+import GetHistories from "@/actions/order/get";
+import { Path } from "@/config/Path";
+import Datatable from "@/components/Datatable";
+
+const HistoryDatatable = () => {
+  const columns = (): GridColDef[] => {
+    return [
+      {
+        field: "created_at",
+        sortable: true,
+        headerName: "วันทำรายการ",
+        flex: 1,
+        editable: false,
+        valueFormatter: (data: any) => ff.date(data.value),
+      },
+      {
+        field: "price",
+        sortable: true,
+        headerName: "ราคา",
+        flex: 1,
+        editable: false,
+        valueFormatter: (data: any) => ff.money(data.value),
+      },
+      {
+        field: "cost",
+        sortable: true,
+        headerName: "ต้นทุน",
+        flex: 1,
+        editable: false,
+        valueFormatter: (data: any) => ff.money(data.value),
+      },
+      {
+        field: "profit",
+        sortable: true,
+        headerName: "กำไร",
+        flex: 1,
+        editable: false,
+        valueFormatter: (data: any) => ff.money(data.value),
+      },
+      {
+        field: "text",
+        sortable: false,
+        headerName: "สินค้า",
+        flex: 1,
+        editable: false,
+      },
+      {
+        field: "note",
+        sortable: true,
+        headerName: "หมายเหตุ",
+        flex: 1,
+        editable: false,
+        valueFormatter: (data: any) => ff.text(data.value),
+      },
+      {
+        field: "actions",
+        type: "actions",
+        headerName: "เครื่องมือ",
+        flex: 1,
+        getActions: ({ row }: { row: Category }) => [
+          <GridLinkAction
+            key="view"
+            to={`${Path("histories").href}/${row.id}`}
+            icon={<ViewAgendaTwoTone />}
+            label="ดูรายละเอียด"
+            showInMenu
+          />,
+        ],
+      },
+    ];
+  };
+
+  return (
+    <>
+      <Datatable
+        name={"histories"}
+        columns={columns()}
+        fetch={GetHistories}
+        height={700}
+      />
+    </>
+  );
+};
+
+export default HistoryDatatable;
