f33146a11b70559cbfa90a4be5622619048416d0
iwindd <freewinendlife@gmail.com>
2024-08-22 02:14:32 +0700

    feat<borrows>: create order history

--- src/actions/borrow/update.ts
+++ src/actions/borrow/update.ts
@@ -24,7 +24,7 @@ const UpdateBorrow = async (
     });
 
     if (data && data.count < data.amount) {
-      await db.product.update({
+      const product = await db.product.update({
         where: {
           id: data.product_id
         },
@@ -32,8 +32,45 @@ const UpdateBorrow = async (
           stock: {
             increment: data.amount - data.count
           }
+        },
+        include: {
+          category: {
+            select: {
+              label: true
+            }
+          }
         }
       })
+
+      if (data.count > 0 && session?.user.store) {
+        const totalPrice = product.price * data.count
+        const totalCost = product.cost * data.count
+        await db.order.create({
+          data: {
+            price: totalPrice,
+            cost: totalCost,
+            profit: totalPrice-totalCost,
+            note: data.note,
+            text: product.label,
+            store_id: +session?.user.store,
+            method: "CASH",
+            type: "BORROW",
+            products: {
+              create: [
+                {
+                  serial: product.serial,
+                  label: product.label,
+                  category: product.category.label,
+                  price: product.price,
+                  cost: product.cost,
+                  count: data.count,
+                  overstock: false
+                }
+              ]
+            }
+          }
+        })
+      }
     }
 
     return { success: true, data: true };
