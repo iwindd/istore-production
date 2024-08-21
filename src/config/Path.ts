1da94904dc8e4f96b9601be03d937909d4df9f36
iwindd <freewinendlife@gmail.com>
2024-08-22 02:25:25 +0700

    feat<path>: nested route

--- src/config/Path.ts
+++ src/config/Path.ts
@@ -6,7 +6,8 @@ export interface Path{
   href: string,
   icon: keyof typeof icons,
   disableBreadcrumb?: boolean,
-  disableNav?: boolean
+  disableNav?: boolean,
+  matcher?: { type: 'startsWith' | 'equals'; href: string };
 }
 
 const Paths = {
@@ -14,10 +15,12 @@ const Paths = {
   'cashier': { key: 'cashier', title: 'ขายสินค้า', href: "/cashier", icon: 'cashier' },
   'products': { key: 'products', title: 'สินค้า', href: "/products", icon: 'product' },
   'borrows': { key: 'borrows', title: 'การเบิก', href: "/borrows", icon: 'borrows' },
-  'purchase': { key: 'purchase', title: 'ซื้อสินค้า', href: "/purchase", icon: 'purchase' },
+  'purchase': { key: 'purchase', title: 'ซื้อสินค้า', href: "/purchase", icon: 'purchase', matcher: { type: 'startsWith', href: "/purchase"} },
+  'purchase.purchase': { key: 'purchase', title: 'รายละเอียดการซื้อสินค้า', href: "/purchase/:pid", icon: 'purchase' , disableNav: true },
   'stock': { key: 'stock', title: 'จัดการสต๊อก', href: "/stocks", icon: 'stock' },
   'categories': { key: 'categories', title: 'ประเภทสินค้า', href: "/categories", icon: 'category' },
-  'histories': { key: 'histories', title: 'ประวัติการขายสินค้า', href: "/histories", icon: 'history' },
+  'histories': { key: 'histories', title: 'ประวัติการขายสินค้า', href: "/histories", icon: 'history', matcher: { type: 'startsWith', href: "/histories"} },
+  'histories.history': { key: 'histories.history', title: 'รายละเอียดการขายสินค้า', href: "/histories/:hid", icon: 'history', disableNav: true },
   'signin': { key: 'signin', title: 'เข้าสู่ระบบ', href: "/auth/signin", icon: 'signin', disableBreadcrumb: true, disableNav: true },
   'signup': { key: 'signup', title: 'ลงทะเบียน', href: "/auth/signout", icon: 'signout', disableBreadcrumb: true, disableNav: true },
 } satisfies Record<string, Path>
