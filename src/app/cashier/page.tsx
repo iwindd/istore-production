1c6bba7db7cc8068666bec2d776665b4ee818bff
iwindd <freewinendlife@gmail.com>
2024-08-26 01:41:10 +0700

    feat<cashier>: add product by selector

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -3,25 +3,56 @@ import React from "react";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import Cashier from "./components/Cashier";
 import { Confirmation, useConfirm } from "@/hooks/use-confirm";
-import { Button, Stack } from "@mui/material";
+import { Button, Divider, Stack, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import { DeleteTwoTone, PaymentTwoTone } from "@mui/icons-material";
 import { useCart } from "@/hooks/use-cart";
 import usePayment from "@/hooks/use-payment";
+import Selector from "@/components/Selector";
+import { Product } from "@prisma/client";
+import { useInterface } from "@/providers/InterfaceProvider";
+import GetProduct from "@/actions/product/find";
+import { enqueueSnackbar } from "notistack";
 
 const CartContainer = dynamic(() => import("./components/Cart"), {
   ssr: false,
 });
 
 const CashierPage = () => {
-  const { clear } = useCart();
+  const { clear, addProduct } = useCart();
   const { Dialog, toggle } = usePayment();
+  const [selectProduct, setSelectProduct] = React.useState<Product | null>(null);
+  const { setBackdrop } = useInterface();
 
   const confirmation = useConfirm({
     title: "แจ้งเตือน",
     text: "คุณต้องการจะล้างตะกร้าหรือไม่? สินค้าภายในตะกร้าจะถูกลบและไม่สามารถย้อนกลับได้!",
     onConfirm: async () => clear(),
   });
+
+  const onSubmit = (Product: Product | null) => {
+    setSelectProduct(Product)
+  }
+
+  const onAddBySelector = async () => {
+    if (!selectProduct) return;
+    try {
+      setBackdrop(true);
+      const resp = await GetProduct(selectProduct.serial);
+      if (!resp.success) throw Error("not_found");
+      addProduct(resp.data as Product);
+      enqueueSnackbar(`เพิ่มสินค้า <${resp.data?.label}> เข้าตะกร้าแล้ว!`, {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar(`ไม่พบสินค้า ${selectProduct.label} ในระบบ!`, {
+        variant: "error",
+      });
+    } finally{
+      setBackdrop(false)
+    }
+    addProduct(selectProduct);
+  }
 
   return (
     <>
@@ -30,6 +61,15 @@ const CashierPage = () => {
           <Cashier />
         </Grid>
         <Grid xs={12} lg={3}>
+          <Stack direction={"row"} spacing={0.3}>
+            <Selector onSubmit={onSubmit} />
+            <Button
+              variant={selectProduct == null ? "text": "contained"}
+              disabled={selectProduct == null}
+              onClick={onAddBySelector}
+            >เพิ่ม</Button>
+          </Stack>
+          <Divider sx={{my: 1}} />
           <Stack spacing={1} direction={"row"}>
             <Button
               variant="contained"
