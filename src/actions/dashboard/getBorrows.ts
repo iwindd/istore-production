27ccd4966aea43f88fd930204bb9044637b51556
iwindd <freewinendlife@gmail.com>
2024-08-27 02:28:42 +0700

    feat<dashboard>: all borrows show, range

--- src/actions/dashboard/getBorrows.ts
+++ src/actions/dashboard/getBorrows.ts
@@ -1,12 +1,14 @@
 "use server";
 import { Borrows } from "@prisma/client";
 import db from "@/libs/db";
+import { getFilterRange } from "./range";
 
 const getBorrows = async (store: number): Promise<Borrows[]> => {
   try {
     return await db.borrows.findMany({
       where: {
         store_id: store,
+        ...await getFilterRange()
       },
     });
   } catch (error) {
