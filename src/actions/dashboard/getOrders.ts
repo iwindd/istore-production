91f969346de1806e73cbd1da01b7404fdf4a3d05
iwindd <freewinendlife@gmail.com>
2024-08-27 02:27:02 +0700

    feat<dashboard>: range

--- src/actions/dashboard/getOrders.ts
+++ src/actions/dashboard/getOrders.ts
@@ -1,12 +1,14 @@
 "use server";
 import { Order } from "@prisma/client";
 import db from "@/libs/db";
+import { getFilterRange } from "./range";
 
 const getOrders = async (store: number): Promise<Order[]> => {
   try {
     return await db.order.findMany({
       where: {
         store_id: store,
+        ...await getFilterRange()
       },
     });
   } catch (error) {
---
+++ src/app/dashboard/range/index.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { AnalyticsTwoTone } from "@mui/icons-material";
+import { Button, Card, CardHeader, Stack, Tooltip } from "@mui/material";
+import { DateField, DatePicker } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
+import React from "react";
+import { useInterface } from "@/providers/InterfaceProvider";
+import { enqueueSnackbar } from "notistack";
+import { RangeChange } from "@/actions/dashboard/range";
+/* import { RangeChange } from "@/actions/dashboard/range";
+ */
+const Range = ({ savedStart, savedEnd }: {
+  savedStart: string | null,
+  savedEnd: string | null
+}) => {
+  const [start, setStart] = React.useState<Dayjs | null>(savedStart ? dayjs(savedStart) : null);
+  const [end, setEnd] = React.useState<Dayjs | null>(savedEnd ? dayjs(savedStart) : null);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await RangeChange(start?.format(), end?.format());
+    } catch (error) {
+      setStart(dayjs().subtract(1, "month"));
+      setStart(dayjs());
+      enqueueSnackbar("เกิดข้อผิดพลาดกรุณาลองใหม่อีกครั้งภายหลัง", {
+        variant: "error",
+      });
+    }
+  };
+
+  return (
+    <Card>
+      <CardHeader
+        title="ภาพรวม"
+        action={
+          <Stack
+            onSubmit={onSubmit}
+            component={"form"}
+            direction={"row"}
+            spacing={1}
+          >
+            <DatePicker
+              value={start}
+              name="start"
+              format="DD/MM/YYYY"
+              onChange={(data) => setStart(data)}
+              label="วันเริมต้น"
+              disableFuture
+            />
+            <DatePicker
+              value={end}
+              name="end"
+              format="DD/MM/YYYY"
+              onChange={(data) => setEnd(data)}
+              label="สิ้นสุด"
+              disableFuture
+            />
+            <Tooltip title="สรุปผล">
+              <Button type="submit" variant="contained">
+                <AnalyticsTwoTone />
+              </Button>
+            </Tooltip>
+          </Stack>
+        }
+      />
+    </Card>
+  );
+};
+
+export default Range;
