c948b427154c3a81f6c6223888a00d4d0f139205
iwindd <freewinendlife@gmail.com>
2024-08-29 07:02:48 +0700

    feat<dayjs>: timezone, utc

--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -1,11 +1,16 @@
 import dayjs, { Dayjs as DJ } from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import isBetween from 'dayjs/plugin/isBetween';
+import timezone from 'dayjs/plugin/timezone';
 import locale from 'dayjs/locale/th';
+import utc from 'dayjs/plugin/utc';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 dayjs.extend(isBetween);
 dayjs.locale(locale)
+dayjs.tz.setDefault("Asia/Bangkok")
 
 export { DJ as Dayjs }; // Exporting the Dayjs type
 export default dayjs; // Exporting the dayjs object
